15821d2ba5a152ed2255a318da8cdd107c96f601
Allan Garcia <allangarcia@usp.br>
2023-11-14 00:44:22 -0300

    python -> python3

--- compile_protobuf.sh
+++ compile_protobuf.sh
@@ -2,7 +2,7 @@ ROOT_PATH=$(dirname $(realpath "$0"))
 PROTO_FILES=$ROOT_PATH/proto/*.proto
 
 echo "Installing protoc"
-python -m pip install grpcio-tools > /dev/null
+python3 -m pip install grpcio-tools > /dev/null
 
 echo "Setting up directories"
 PY_OUT_MODULE=$ROOT_PATH/backend/proto
@@ -13,8 +13,8 @@ mkdir -p $PY_OUT_MODULE $ESP_OUTPUT_INCLUDE $ESP_OUTPUT_SRC
 
 echo "Building for Python"
 touch $PY_OUT_MODULE/__init__.py
-python -m grpc.tools.protoc $PROTO_FILES -I $ROOT_PATH --python_out=$PY_OUT_MODULE/.. --pyi_out=$PY_OUT_MODULE/.. 2> /dev/null
+python3 -m grpc.tools.protoc $PROTO_FILES -I $ROOT_PATH --python_out=$PY_OUT_MODULE/.. --pyi_out=$PY_OUT_MODULE/.. 2> /dev/null
 
 echo "Building for ESP32"
-python -m grpc.tools.protoc $PROTO_FILES -I $ROOT_PATH --nanopb_out=$ESP_OUTPUT_INCLUDE/.. 2> /dev/null
+python3 -m grpc.tools.protoc $PROTO_FILES -I $ROOT_PATH --nanopb_out=$ESP_OUTPUT_INCLUDE/.. 2> /dev/null
 mv $ESP_OUTPUT_INCLUDE/*.c $ESP_OUTPUT_SRC
